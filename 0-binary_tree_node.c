8dbff65b6ea39c31ec0a25aab14ff0626d8ade39
71Arid <brian87noel@gmail.com>
2023-06-10 19:26:11 +0300

    ii

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -5,26 +5,31 @@
  * parent - pointer to parent node of node to create
  * @value: value of the node to create
  */
- 
- binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
- {
-     if (parent == NULL || value == NULL || )
-     {
-         return NULL;
-     }
-     binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
-     newNode->n = value;
-     newNode->left = NULL;
-     newNode->right = NULL;
-     if (value > parent->n){
-        parent->right = newNode;
-     }
-     else if (value < parent->n)
-     {
-         parent->left = newnode;
-     }
-     else 
-     {
-         return(parent);
-     }
- }
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	if (parent == NULL)
+	{
+		return NULL;
+	}
+	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+	if (newNode == NULL)
+	{
+		return NULL;
+	}
+	newNode->n = value;
+	newNode->left = NULL;
+	newNode->right = NULL;
+	if (value > parent->n){
+		parent->right = newNode;
+	}
+	else if (value < parent->n)
+	{
+		parent->left = newNode;
+	}
+	else 
+	{
+		return(newNode);
+	}
+	return (newNode);
+}
