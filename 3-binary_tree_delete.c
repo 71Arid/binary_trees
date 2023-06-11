d92f5a7bc2faf3da412c1b56d0c58635a5e1ad09
71Arid <brian87noel@gmail.com>
2023-06-11 15:35:30 +0300

    ii

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * void brinary_tree_delete - deletes a binary tree
+ * @tree: pointer to root of tree to delete
+ * Return: NULL if tree is null
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	free(tree);
+}
